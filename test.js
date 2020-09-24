984b60aeb9a0c1738278328c2d75b2e7fee2b666
Danny Sullivan <dbsullivan23@gmail.com>
2020-09-24 13:30:54 -0500

    adding second test

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-xdescribe('some test', () => {
-  test('some test', () => {
+describe('some test', () => {
+  xtest('some test', () => {
     console.log('this is a test')
   })
+  // if you uncomment this, the test will pass, because afterAll is run and closes the open connection
+  // test('some test', () => {
+  //   console.log('this is a test')
+  // })
 })
